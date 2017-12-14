1f287a075b38d108b1b7832ce4b5de7579944dc8
Joseph Rolli <rollij@aston.ac.uk>
2017-12-14 20:57:30 +0000

    Documentation

--- src/mtr/TrainLine.java
+++ src/mtr/TrainLine.java
@@ -1,58 +1,99 @@
 package mtr;
 
 import java.util.LinkedHashSet;
-import java.util.HashSet;
 
-
+/**
+ * Represents a specific trainline in a rail network. Extends
+ * {@linkLinkedHashSet}
+ * 
+ * 
+ * @author Jawwad Choudhury
+ * @author Hassan Hussein
+ * @author Joseph Rolli
+ * 
+ */
 public class TrainLine extends LinkedHashSet<String> {
 
+	/**
+	 * The {@link String} representing the name of the line.
+	 */
 	private String lineName;
+
+	/**
+	 * The {@link String} representing the name of the first station in the line.
+	 */
 	private String firstStation;
+
+	/**
+	 * The {@link String} representing the name of the last station in the line.
+	 */
 	private String lastStation;
-	private HashSet<String> adjacentStations;
 
 	public TrainLine() {
 
 	}
 
-	public String getLineName() {
-		return lineName;
-	}
-
+	/**
+	 * Sets the name of the train line.
+	 * 
+	 * @param lineName
+	 */
 	public void setLineName(String lineName) {
 		this.lineName = lineName;
 	}
 
-	public String getFirstStation() {
-		return firstStation;
-	}
-
+	/**
+	 * Sets the name of the first station in the line.
+	 * 
+	 * @param firstStation
+	 */
 	public void setFirstStation(String firstStation) {
 		this.firstStation = firstStation;
 	}
 
+	/**
+	 * Sets the name of the last station in the line.
+	 * 
+	 * @param lastStation
+	 */
+	public void setLastStation(String lastStation) {
+		this.lastStation = lastStation;
+	}
+
+	/**
+	 * Retrieves the line name.
+	 * 
+	 * @return <code>String</code>
+	 */
+	public String getLineName() {
+		return lineName;
+	}
+
+	/**
+	 * Retrieves the name of the first station.
+	 * 
+	 * @return <code>String</code>
+	 */
+	public String getFirstStation() {
+		return firstStation;
+	}
+
+	/**
+	 * Retrieves the name of the last station.
+	 * 
+	 * @return <code>String</code>
+	 */
 	public String getLastStation() {
 		return lastStation;
 	}
 
-	public void setLastStation(String lastStation) {
-		this.lastStation = lastStation;
-	}
-	
-	public void addAdjacentStation(String station) {
-		adjacentStations.add(station);
-	}
-	
-	public String getAdjacentStations() {
-		String stations = null;
-		for (String station : adjacentStations)
-			stations = station + " ";
-		return stations;
-	}
-	
+	/**
+	 * Retrieves the name representing this object..
+	 * 
+	 * @return <code>String</code>
+	 */
 	public String toString() {
 		return getLineName();
 	}
-	
 
 }
